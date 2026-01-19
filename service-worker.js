bfbd724749e7c9a4846e825c5143a23457f9f246
ravisahariya <ravisahariya3.bzy@gmail.com>
2026-01-19 11:54:10 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("digital-register-v1").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./page2.html"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
