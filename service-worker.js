90b0dd8e775bb32530ced4fdc935394d43b0c663
RAVI SAHARIYA <ravisahariya3.bzy@gmail.com>
2026-01-25 16:28:41 +0530

    Update service-worker.js
    Signed-off-by: RAVI SAHARIYA <ravisahariya3.bzy@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,16 @@
 self.addEventListener("install",e=>{
- e.waitUntil(
-  caches.open("user-data").then(c=>c.addAll([
-   "./","index.html","admin.html","arvind.html","manifest.json"
-  ]))
- );
+  e.waitUntil(
+    caches.open("app").then(c=>
+      c.addAll([
+        "index.html",
+        "admin.html",
+        "arvind.html"
+      ])
+    )
+  );
 });
 self.addEventListener("fetch",e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
 });
