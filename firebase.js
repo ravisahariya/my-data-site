c2369d071224e177bdb7c3597a31844f510d0095
RAVI SAHARIYA <ravisahariya3.bzy@gmail.com>
2026-02-01 06:50:15 +0530

    Update firebase.js
    Signed-off-by: RAVI SAHARIYA <ravisahariya3.bzy@gmail.com>

--- firebase.js
+++ firebase.js
@@ -1,11 +1,13 @@
 const firebaseConfig = {
-apiKey:"YOUR_KEY",
-authDomain:"YOUR_DOMAIN",
-projectId:"YOUR_ID",
-storageBucket:"YOUR_BUCKET",
-messagingSenderId:"YOUR_ID",
-appId:"YOUR_ID"
+  apiKey: "AIzaSyD71mpolAsmejtU2icecQQcX07iCW_is0Y",
+  authDomain: "ravicardmaker.firebaseapp.com",
+  projectId: "ravicardmaker",
+  storageBucket: "ravicardmaker.firebasestorage.app",
+  messagingSenderId: "125321734592",
+  appId: "1:125321734592:web:fc0cfe4d8e85ee80134025",
+  measurementId: "G-EVDWN80G38"
 };
 
 firebase.initializeApp(firebaseConfig);
+
 const db = firebase.firestore();
